d507811be44f697163e24b8c772673aec548a548
naza-campioni <n_a_z_a_r_e_n_o@hotmail.com>
2026-03-26 11:26:38 +0100

    multi analysis

---
+++ scripts/season_analysis.R
@@ -0,0 +1,59 @@
+library(here)
+
+source(here("R/load_packages.R"))
+source(here("R/load_shapefiles.R"))
+source(here("R/load_all.R"))
+
+load_packages()
+
+env_path <- list("aligned_mean_season_rasters_2025/WINSPR",
+                 "aligned_mean_season_rasters_2025/SUMAUT")
+
+shapefiles <- load_shapefiles()
+med_poly <- shapefiles$med
+regions <- shapefiles$reg
+regions$med <- med_poly
+
+name_files <- list('data/season_data/sea_df_winspr.csv',
+                   'data/season_data/sea_df_sumaut.csv')
+folders <- list("season/win_spr", "season/sum_aut")
+
+config <- list(
+  # paths
+  res.dir = "experiments",
+  base_folder = "",
+  
+  # data
+  occ_file = "",
+  env = env,
+  regions = regions,
+  
+  # model settings
+  partitions = c("block", "checkerboard", "checkerboard"),
+  partition.folders = c("block", "checkerboard", "hierarchical_checkerboard"),
+  ag = list(NULL, 10, c(10,10)),
+  
+  fc = c('L','Q','P','LQ','H'),
+  rm = seq(1,5,0.5),
+  
+  # metadata
+  year.range = "2015_2025",
+  thin = FALSE,
+  parallel = FALSE,
+  
+  # bias
+  bandwidth = c(20000)
+)
+
+for (i in seq_along(name_files)) {
+  env <- load_env(env_path[[i]])
+  env_polished <- calculate_vif(env)
+  env <- env_polished$env
+  env.vif <- env_polished$vif
+  env.rem <- env_polished$rem
+  
+  config$occ_file <- name_files[[i]]
+  config$base_folder <- folders[[i]]
+  
+  run_pipeline(config)
+}
